4b82fa5f982742c7456c5e90510b7c78285c258d
Prashant Kumar <programmerkrprashant@gmail.com>
2024-05-05 22:41:03 +0530

    Light And Dark mode

--- mini JS Projects/LightAndDark Mode/script.js
+++ mini JS Projects/LightAndDark Mode/script.js
@@ -2,14 +2,23 @@ const parentCheckBox = document.querySelector('#parentCheckBox');
 const container = document.querySelector('.container');
 const contatnTheme = document.querySelector('.contatnTheme');
 
+const storedTheme = localStorage.getItem('theme')
+if (storedTheme) {
+    parentCheckBox.checked = true;
+    contatnTheme.checked = true;
+    container.classList.add(storedTheme)
+}
+
 parentCheckBox.addEventListener('click', function () {
     if (parentCheckBox.checked) {
-        contatnTheme.click();
-        container.classList.add('theme')
+        contatnTheme.checked = true;
+        container.classList.add('dark')
+        localStorage.setItem('theme', 'dark')
 
     } else {
-        contatnTheme.click();
-        container.classList.remove('theme')
+        contatnTheme.checked = false;
+        container.classList.remove('dark')
+        localStorage.removeItem('theme');
 
     }
 })
