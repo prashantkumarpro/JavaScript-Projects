6a75f2b4fcabf32c514f2d552f054f9d3e37778a
Prashant Kumar <programmerkrprashant@gmail.com>
2024-05-09 22:56:10 +0530

    AutoTyping Text

---
+++ mini JS Projects/AutoTypingTest/script.js
@@ -0,0 +1,35 @@
+const typingText = document.querySelector('.typingText');
+const words = ['Developer.', 'Coder.', 'Teacher.']
+
+
+let word = words[0];
+let index = 0;
+
+function typedAndRemove(word, index) {
+    setInterval(() => {
+        if (index < word.length) {
+            typingText.innerHTML = typingText.innerHTML + word[index];
+            index++;
+        } 
+        else {
+            index = 0;
+            typingText.innerHTML = "";
+
+        }
+
+
+    }, 200)
+}
+typedAndRemove(word, index)
+
+
+
+
+
+
+
+
+
+
+
+
