2612f0e050b26d55784155ecff80cd59d004c92a
Prashant Kumar <programmerkrprashant@gmail.com>
2024-05-18 17:32:22 +0530

    Bill Split Final

--- mini JS Projects/Bill Split/script.js
+++ mini JS Projects/Bill Split/script.js
@@ -1,138 +1,210 @@
-const billINput = document.querySelector('#billINput');
+// Selecting DOM elements
+const textInputs = document.querySelectorAll('input[type="text"]');
+const billInput = document.querySelector('#billInput');
 const selectTipsBtn = document.querySelectorAll('.selectTip');
 const customTipInput = document.querySelector('#custom_Tip');
 const noOfPeopleInput = document.querySelector('#noOfPeople');
-const generateBillInput = document.querySelector('#generateBill');
+const generateBillBtn = document.querySelector('#generateBill');
 const restBtn = document.querySelector('#rest');
 
+// Initializing variables
 let billInputValue;
 let noOfPeopleValue;
 let selectedTipAmount = 0;
 let customTipAmount = 0;
 let currentTipAmount = 0;
 
-const textInputs = document.querySelectorAll('input[type="text"]');
 
-
+// Event listener for text input fields
 textInputs.forEach(function (input) {
     input.addEventListener('input', function () {
         let inputValue = this.value;
         let numbersOnly = /^\d+$/;
         if (!inputValue.match(numbersOnly)) {
+            // Clear input if non-numeric value is entered
             this.value = '';
-            if (this.className.includes('billInput')) {
+
+            if (this.id === 'billInput') {
+                // Disable relevant inputs and buttons if bill input is empty
                 disable(customTipInput)
                 disable(noOfPeopleInput)
-                selectTipsBtn.forEach((tip) => {
-                    disable(tip);
-                    tip.classList.remove('enable');
-                })
+                disableTipButtons()
+            }
+
+
+            if (this.id === 'custom_Tip') {
+                // Disable Generate Bill button if custom tip input is empty
+                disableGenerateBillBtn()
+            }
+
+            if (this.id === 'noOfPeople') {
+                // Disable Generate Bill button if number of people input is empty
+                disableGenerateBillBtn()
             }
         } else {
-            // Input is valid
-            if (this.className.includes('billInput')) {
+
+            if (this.id === 'billInput') {
+                // Enable relevant inputs and buttons if bill input is valid
                 billInputValue = parseInt(this.value);
                 enabled(customTipInput)
                 enabled(noOfPeopleInput)
-                selectTipsBtn.forEach((tip) => {
-                    enabled(tip)
-                    tip.classList.add('enable');
-                })
+                enableTipButtons()
             }
 
-            if (this.className.includes('custom_Tip')) {
-                handleSelectedTip(0)
+            if (this.id === 'custom_Tip') {
+                // Enable Generate Bill button if custom tip input and number of people are valid
+                handleSelectedTip(0);
+                selectTipsBtn.forEach(el => el.classList.remove('active'));
                 customTipAmount = parseInt(this.value);
                 currentTipAmount = customTipAmount;
+                if (noOfPeopleValue > 0 && currentTipAmount > 0) {
+                    enableGenerateBillBtn()
+                }
+                if (noOfPeopleInput.value === '' && currentTipAmount > 0) {
+                    // Disable Generate Bill button if number of people input value is empty and tip are vaild
+                    disableGenerateBillBtn()
+                }
             }
 
-            if (this.className.includes('noOfPeople')) {
+            if (this.id === 'noOfPeople') {
+                // Enable Generate Bill button if number of people input and custom tip are valid
                 noOfPeopleValue = parseInt(this.value);
-                console.log(currentTipAmount)
+                if (noOfPeopleValue > 0 && currentTipAmount > 0) {
+                    enableGenerateBillBtn()
+
+                }
+                // Disable Generate Bill button if number of people input is 0
+                if (noOfPeopleValue === 0) {
+                    disableGenerateBillBtn()
+                }
             }
-            if (currentTipAmount && noOfPeopleValue) {
-                enabled(generateBillInput);
-                generateBillInput.classList.add('enable');
-            }
+
 
         }
     });
 });
 
 
+
+
+
+// Event listener for tip buttons
 selectTipsBtn.forEach((tip) => {
-    disableTipBtn(tip);
-    enableTipBtn(tip);
 
     tip.addEventListener('click', (e) => {
-        currentTipAmount = parseInt(e.target.value);
-        handleSelectedTip(currentTipAmount)
+
+        selectedTipAmount = parseInt(e.target.value);
+        currentTipAmount = selectedTipAmount;
+        handleSelectedTip(currentTipAmount);
+        selectTipsBtn.forEach(el => el.classList.remove('active'));
+        e.target.classList.add('active');
+        if (customTipInput.value !== '') {
+            // Clear custom tip input and reset custom tip amount if a tip button is clicked
+            customTipInput.value = '';
+            customTipAmount = 0;
+            currentTipAmount = selectedTipAmount;
+            handleSelectedTip(currentTipAmount);
+
+            e.target.classList.remove('active');
+        }
+
+        if (customTipInput.value === '' && e) {
+            e.target.classList.add('active');
+        }
+
+        if (noOfPeopleValue > 0 && currentTipAmount > 0) {
+            // Enable Generate Bill button if number of people input and tip are valid
+            enableGenerateBillBtn()
+        }
+
+        if (noOfPeopleInput.value === '' && currentTipAmount > 0) {
+            // Disable Generate Bill button if number of people input is empty
+            disableGenerateBillBtn()
+        }
+
+
     })
 });
 
-function disableTipBtn(tip) {
-    disable(tip);
-    tip.classList.remove('enable');
-};
 
-function enableTipBtn(tip) {
-    enabled(tip);
-    tip.classList.remove('enable');
-};
 
+// Function to handle disable tip buttons
+function disableTipButtons() {
+    selectTipsBtn.forEach(tipButton => {
+        disable(tipButton)
+        tipButton.classList.remove('enable')
+    })
+}
+
+// Function to handle enable tip buttons
+function enableTipButtons() {
+    selectTipsBtn.forEach(tipButton => {
+        enabled(tipButton)
+        tipButton.classList.add('enable')
+    })
+}
+
+// Function to enable generate bill button
+function enableGenerateBillBtn() {
+    enabled(generateBillBtn);
+    generateBillBtn.classList.add('enable');
+}
+// Function to disable generate bill button
+function disableGenerateBillBtn() {
+    disable(generateBillBtn);
+    generateBillBtn.classList.remove('enable');
+}
+// Function to handle selected tip
 function handleSelectedTip(currentTipAmount) {
-    console.log(`This is selectedTipAmount ${currentTipAmount}`)
+    // console.log(currentTipAmount)
 };
 
-// generate bill 
+// Function to generate bill
 function generateBill() {
     let resultOfTipAmount = (billInputValue * currentTipAmount) / 100;
     let resultOfTotalAmount = billInputValue + resultOfTipAmount;
     let resultOfEachPersonBill = resultOfTotalAmount / noOfPeopleValue;
-    document.querySelector('#tipAmount').innerText = `₹ ${resultOfTipAmount.toFixed(2)}`
-    document.querySelector('#totalAmount').innerText = `₹ ${resultOfTotalAmount.toFixed(2)}`
-    document.querySelector('#eachPersonBill').innerText = `₹ ${resultOfEachPersonBill.toFixed(2)}`
-    enabled(restBtn)
-
-
+    // Updating UI with bill details
+    document.querySelector('#tipAmount').innerHTML = `₹ ${resultOfTipAmount.toFixed(2)}`;
+    document.querySelector('#totalAmount').innerHTML = `₹ ${resultOfTotalAmount.toFixed(2)}`
+    document.querySelector('#eachPersonBill').innerHTML = `₹ ${resultOfEachPersonBill.toFixed(2)}`
+    enabled(restBtn);
 };
 
 
-// click event on generat bill input button
-generateBillInput.addEventListener('click', generateBill);
+// Event listener for Generate Bill button
+generateBillBtn.addEventListener('click', generateBill);
 
-// rest the value input box and result amount to empty
+// Event listener for Reset button
 restBtn.addEventListener('click', function () {
+    selectTipsBtn.forEach(el => { el.classList.remove('active') });
     resetTheValue()
     disable(restBtn)
 });
 
+// Function to reset input values and disable relevant elements
 function resetTheValue() {
     document.querySelector('#tipAmount').innerText = ''
     document.querySelector('#totalAmount').innerText = ''
     document.querySelector('#eachPersonBill').innerText = ''
-    billINput.value = '';
+    billInput.value = '';
     customTipInput.value = '';
     noOfPeopleInput.value = '';
-    currentTipAmount = '';
-    customTipAmount = '';
-    noOfPeopleValue = '';
-    disable(generateBillInput)
+    currentTipAmount = 0;
+    customTipAmount = 0;
+    noOfPeopleValue = 0;
     disable(customTipInput)
     disable(noOfPeopleInput)
-    generateBillInput.classList.remove('enable')
-    selectTipsBtn.forEach((tip) => {
-        disable(tip)
-        tip.classList.remove('enable');
-    })
+    disableTipButtons()
+    disableGenerateBillBtn()
 };
 
-// enable the input 
+// Function to enable an element
 function enabled(element) {
     element.disabled = false;
 };
 
-// disable the input 
+// Function to disable an element
 function disable(element) {
     element.disabled = true;
 };
