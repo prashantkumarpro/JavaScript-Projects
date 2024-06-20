659d797015f62767481ff797c1356f410f04984b
Prashant Kumar <programmerkrprashant@gmail.com>
2024-06-20 16:39:48 +0530

    Faq page

---
+++ mini JS Projects/Faq page/script.js
@@ -0,0 +1,20 @@
+
+const sections = document.querySelectorAll('.section')
+    .forEach(section => {
+        section.addEventListener('click', function (e) {
+            if (e.currentTarget) {
+                const contentElem = e.currentTarget.children[1];
+                const signElem = e.currentTarget.children[0].children[1];
+
+                if (contentElem.classList.contains('active')) {
+                    contentElem.classList.remove('active')
+                    contentElem.style.maxHeight = 0;
+                    signElem.innerText = '+'
+                } else {
+                    contentElem.classList.add('active')
+                    contentElem.style.maxHeight = contentElem.scrollHeight + 'px'
+                    signElem.innerText = '-'
+                };
+            }
+        })
+    })
