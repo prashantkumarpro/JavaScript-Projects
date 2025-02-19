27b9727065a9b04f96aa7be750a031f0301f6813
Prashant Kumar <programmerkrprashant@gmail.com>
2025-02-19 23:05:00 +0530

    quize quesitons data module

---
+++ mini JS Projects/QuizeWebApp/data.js
@@ -0,0 +1,41 @@
+// Quiz questions data module
+const quizQuestions = [
+  {
+    Qns: 'Who was the first Prime Minister of India?',
+    correctAns: 'Pandit Nehru',
+    options: [
+      'Pandit Nehru',
+      'Dr. Rajendra Prasad',
+      'Dr. B.R. Ambedkar',
+      'Lal Bahadur Shastri'
+    ]
+  },
+  {
+    Qns: 'What is the capital of France?',
+    correctAns: 'Paris',
+    options: ['London', 'Berlin', 'Paris', 'Madrid']
+  },
+  {
+    Qns: 'Which planet is known as the Red Planet?',
+    correctAns: 'Mars',
+    options: ['Venus', 'Jupiter', 'Mars', 'Saturn']
+  },
+  {
+    Qns: 'Who invented the light bulb?',
+    correctAns: 'Thomas Edison',
+    options: [
+      'Nikola Tesla',
+      'Albert Einstein',
+      'Thomas Edison',
+      'Isaac Newton'
+    ]
+  },
+  {
+    Qns: 'Which is the largest ocean on Earth?',
+    correctAns: 'Pacific Ocean',
+    options: ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean']
+  }
+]
+
+// Export the quizQuestions array for use in script.js
+export default quizQuestions
