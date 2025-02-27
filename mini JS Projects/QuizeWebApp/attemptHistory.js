10866cc9ce3a3e89c04727cb1c9aee889a16c791
Prashant Kumar <programmerkrprashant@gmail.com>
2025-02-27 22:31:30 +0530

    fetched DB and updated UI

--- mini JS Projects/QuizeWebApp/attemptHistory.js
+++ mini JS Projects/QuizeWebApp/attemptHistory.js
@@ -1,4 +1,33 @@
-import { currentQuestion } from "./script.js"
+let fetchedData = []
 
+let db
 
-console.log(currentQuestion)
+// open the indexedDB
+const DBopenRequest = indexedDB.open('attemptsHistory', 1)
+
+DBopenRequest.onsuccess = event => {
+  db = event.target.result
+  console.log('IndexedDB is running now', db)
+  fetchAttempts(db)
+}
+
+// Function to fetch all past attempts from IndexedDB
+const fetchAttempts = db => {
+  const transaction = db.transaction('attemptsHistory', 'readonly')
+  const store = transaction.objectStore('attemptsHistory')
+  const getAllRequest = store.getAll()
+
+  // When data is retrieved successfully, update the fetchedData
+  getAllRequest.onsuccess = () => {
+    fetchedData = getAllRequest.result
+    if (fetchedData.length > 0) {
+      fetchedData.reverse().map((attemp, index) => {
+        document.getElementById('attempt').innerHTML += `
+        <li>Attempt ${index + 1} : ${attemp.score}/5 </li>
+        `
+      })
+    } else {
+      // console.log(fetchedData)
+    }
+  }
+}
